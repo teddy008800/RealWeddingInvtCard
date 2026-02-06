6aa936270c9f5ef8a013805f4e5244260836692d
Adanan,Aqil Husain <aqil.adanan@ankaaconsulting.com>
2026-02-06 21:17:44 +0800

    fix not view

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.html',
   standalone: false,
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected title = 'RealWeddingInvtCard';
